1735e9b3e6712c41a64ba6364f7e580dc3b28049
Lennart Centen <splie30@hotmail.com>
2023-03-15 19:34:29 +0100

    Initial Commit

---
+++ NineGame/NineGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NineGameApp.swift
+//  NineGame
+//
+//  Created by José  Broeks on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NineGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
